5d378c7b0c99c32097ad4086e29001e5afbae2b9
wuxiaoran <wuxiaoran@xinzhili.cn>
2021-09-26 17:16:19 +0800

    build(build): 去掉图片压缩:构建时间过长

--- packages/shared/image.mjs
+++ packages/shared/image.mjs
@@ -1,15 +1,15 @@
 import imagemin from 'imagemin';
-import imageminJpegtran from 'imagemin-jpegtran';
-import imageminPngquant from 'imagemin-pngquant';
+// import imageminJpegtran from 'imagemin-jpegtran';
+// import imageminPngquant from 'imagemin-pngquant';
 
 (async () => {
   const files = await imagemin(['./src/assets/**/*.(jpg|jpeg|png|svg)'], {
     destination: './dist/src/assets/',
     plugins: [
-      imageminJpegtran(),
-      imageminPngquant({
-        quality: [0.6, 0.8],
-      }),
+      // imageminJpegtran(),
+      // imageminPngquant({
+      //   quality: [0.6, 0.8],
+      // }),
     ],
   });
   console.log(files);
