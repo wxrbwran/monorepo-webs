077be9abeeb88dac21012184483da4d04b0c9dc2
gx <gaoxue@xinzhili.cn>
2021-09-28 20:05:51 +0800

    fix: fix

--- packages/doctor/src/components/CheckImgStructured/compontents/TopicTitle/index.tsx
+++ packages/doctor/src/components/CheckImgStructured/compontents/TopicTitle/index.tsx
@@ -8,7 +8,7 @@ interface IProps {
 const TopicTitle: FC<IProps> = (props) => {
   const { number, btnText, handleAdd } = props;
   return (
-    <div className="flex justify-between mb-5">
+    <div className="flex justify-between mb-5  structured-edit-wrap">
       <span className="text-lg font-bold">{number}. </span>
       <span
         className="text-blue-400 cursor-pointer"
