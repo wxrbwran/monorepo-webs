33a179e26dad4f645dade45fe6f4fecf7e5b110a
gx <gaoxue@xinzhili.cn>
2021-09-29 20:50:45 +0800

    feat: 护士端更新数据结构化

---
+++ packages/nurse/src/pages/checked_img/[checked]/structured/compontents/TopicProblem/index.tsx
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import delIcon from '@/assets/img/doctor_patients/delete-icon.svg';
+import { Input, message } from 'antd';
+import { IQuestions } from 'typings/imgStructured';
+import TopicTitle from '../TopicTitle';
+import { EditOutlined } from '@ant-design/icons';
+import { isEmpty, cloneDeep, debounce } from 'lodash';
+import { textData } from '../../utils';
+
+const { TextArea } = Input;
+interface IProps {
+  changeCallbackFns: (params: ICallbackFn) => void;
+  initData: IQuestions[];
+  isViewOnly: boolean;
+}
+function TopicProblem(props: IProps) {
+  const { changeCallbackFns, initData, isViewOnly } = props;
+  console.log('initDatatext', initData);
+  const [questions, setQuestions] = useState<IQuestions[]>(initData ? initData : []);
+  const [editIndex, setEditIndex] = useState(-1);
+  const handleSave = () => new Promise((resolve) => {
+    // resolve(fetchSubmitData(questions, 3));
+    resolve({
+      data: questions.filter(item => !!item.question.trim()),
+      groupInx: 3,
+    });
+  });
+  useEffect(() => {
+    if (initData && isEmpty(questions)) {
+      setQuestions(initData);
+    }
+  }, [initData]);
+  useEffect(() => {
+    if (changeCallbackFns) {
+      changeCallbackFns({
+        type: 'TEXT',
+        fn: handleSave,
+      });
+    }
+  }, [questions]);
+  const changeEditIndex = () => {
+    if (editIndex !== -1 && editIndex !== 999) {
+      if (!isEmpty(questions)) {
+        const { question, answer } = questions[editIndex];
+        // 如果问题和答案都是空，则删除此项
+        if (question.trim() === '' && isEmpty(answer)) {
+          questions.splice(editIndex, 1);
+          setQuestions(questions);
+          setEditIndex(999);
+        } else if (question.trim() === '') {
+          message.error('请输入问题');
+        } else {
+          setEditIndex(999);
+        }
+      }
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('click', changeEditIndex);
+    return () => {
+      window.removeEventListener('click', changeEditIndex);
+    };
+  }, [editIndex]);
+  const handleDelStem = (inx: number) => {
+    questions.splice(inx, 1);
+    setQuestions([...questions]);
+    setEditIndex(999);
+  };
+  const handleAddTopic = (e: Event) => {
+    e.stopPropagation();
+    const inx = questions.length;
+    setQuestions([...questions, cloneDeep(textData)]);
+    setEditIndex(inx);
+  };
+  const handleClickItem = (e: Event, inx: number) => {
+    e.stopPropagation();
+    setEditIndex(inx);
+  };
+  const handleSaveStem = (ev: React.ChangeEvent<HTMLInputElement>, quesIndex: number) => {
+    questions[quesIndex].question = ev.target.value;
+    setQuestions([...questions]);
+  };
+  const handleSaveAnswer = (ev: React.ChangeEvent<HTMLInputElement>, quesIndex: number) => {
+    questions[quesIndex].answer = [ev.target.value];
+    setQuestions([...questions]);
+  };
+  console.log('最新question', questions);
+  let emptyAnsNum = 0;
+  return (
+    <div className="border p-15 my-15">
+      <TopicTitle number="三" handleAdd={debounce(handleAddTopic, 300)} btnText='添加新的问答题' />
+      <div className="qa-wrap">
+        {
+          questions.map((item, quesIndex: number) => {
+            const isEdit = editIndex === quesIndex;
+            let isShow = true;
+            if (isViewOnly && !item.answer[0]?.trim()) {
+              isShow = false;
+              ++emptyAnsNum;
+            }
+            return (
+              isShow ? (
+                <div
+                  className={`${(isEdit) ? 'edit topic-item' : ' topic-item-show'}`}
+                  key={quesIndex}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  {
+                    isEdit ? (
+                      <>
+                        <div className="bg-gray-50 mb-5 p-8">
+                          <div className="mr-40 pl-5">示例：影像表现</div>
+                          <div className="border p-5"> 膀胱充盈良好，膀胱充盈良好 </div>
+                        </div>
+                        <div className={`issue ${!!item.question ? '' : 'input-empty'} pl0`}>
+                          <span className="issue__index">{quesIndex - emptyAnsNum + 1}、</span>
+                          <Input
+                            placeholder="请输入问题"
+                            value={item.question}
+                            onChange={(ev: any) => handleSaveStem(ev, quesIndex)}
+                          />
+                          <img className="issue__delete" src={delIcon} onClick={() => handleDelStem(quesIndex)} />
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        {
+                          item.isAdd && <EditOutlined onClick={(e: any) => handleClickItem(e, quesIndex)} />
+                        }
+                        <div className="pl-12">{quesIndex - emptyAnsNum + 1}、{item.question}</div>
+                      </>
+                    )
+                  }
+                  <div className="answer-wrap">
+                    <TextArea
+                      placeholder="请输入"
+                      onChange={(ev: any) => handleSaveAnswer(ev, quesIndex)}
+                      value={item.answer?.[0]}
+                      disabled={isViewOnly}
+                    />
+                  </div>
+                </div>
+              ) : <></>
+            );
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default TopicProblem;
--- packages/nurse/src/pages/checked_img/[checked]/structured/index.tsx
+++ packages/nurse/src/pages/checked_img/[checked]/structured/index.tsx
@@ -1,41 +1,150 @@
-import React, { FC, useEffect, useState } from 'react';
-import { history } from 'umi';
-import { isEmpty } from 'lodash';
+import React, { useState, FC, useEffect } from 'react';
 import { Spin } from 'antd';
+import { IApiDocumentList, IImgStructuredApiData, IMeta, ITmpList, ITopicItemApi, ITopicTemplateItemApi } from 'typings/imgStructured';
 import * as api from '@/services/api';
-import StructuredDetail from './compontents/StructuredDetail';
 import ImgWrap from './compontents/ImgWrap';
+import StructuredDetail from './compontents/StructuredDetail';
+import { ExclamationCircleFilled } from '@ant-design/icons';
+import { isEmpty } from 'lodash';
+import uuid from 'react-uuid';
+import { history } from 'umi';
 
-interface IData {
-  list: IImgTypeItems[],
-  url: string;
-  imageOutType?: string;
-  degree: number;
+interface IProps {
+  handleRefresh?: () => void;
 }
-const Structured: FC = () => {
-  const [data, setData] = useState<IData>({});
+interface ITempItem {
+  data: ITopicTemplateItemApi;
+  meta: IMeta;
+}
+const CheckImgStructured: FC<IProps> = (props) => {
+  const { handleRefresh } = props;
   const { imageId } = history.location.query;
-  useEffect(() => {
+  const [hydData, setHydData] = useState<IApiDocumentList[]>([]);
+  const [jcdData, setJcdData] = useState<ITopicItemApi[]>([]);
+  const [imgData, setImgData] = useState<IImgStructuredApiData>();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [openTime, setOpenTime] = useState(new Date().getTime()); // 打开图片结构化的时间
+  // 编辑：后增加的模板
+  const [templatePart, setTemplatePart] = useState<ITmpList>({});
+  // 全部模板 from ： 0
+  const [tempAll, settempAll] = useState<ITmpList>({});
+  const patientSid = window.$storage.getItem('patientSid');
+
+  // 获取问题模板: 取上面接口返回的第一个list里的createtime  ，list为空表示是初次， form传0
+  const fetchTemplate = (from: number, to: number) => {
+    const params = { from, to: to || openTime };
+    api.image.fetchImageTopicTemplate(params).then(res => {
+      const tempData: ITmpList = {};
+      res.list.forEach((item: ITempItem) => {
+        const type = item.meta.title;
+        if (!tempData[type]) {
+          tempData[type] = [];
+        }
+        tempData[type] = tempData[type].concat(item.data);
+      });
+      if (from) {
+        setTemplatePart(tempData);
+        setIsLoaded(true);
+      } else {
+        settempAll(tempData);
+      }
+    });
+  };
+  const fetchImageJcds = async () => {
+    const params = { meta: { imageId,  sid: patientSid } };
+    const data = api.image.fetchImageJcdAndOther(params);
+    return data;
+  };
+  const fetchImageIndexes = async () => {
     const params = {
       imageId,
-      patientSid: window.$storage.getItem('patientSid'),
+      patientSid: patientSid,
+      operatorId: window.$storage.getItem('sid'),
+      wcId: window.$storage.getItem('wcId'),
     };
-    console.log('paramsd', params);
-    api.image.fetchImageIndexes(params).then((res) => {
-      setData(res);
-    });
-  }, []);
-  return (
-    <>
-      {
-        !isEmpty(data) ? (
-          <div className="flex justify-start items-start mt-10" style={{ minWidth: 1200 }}>
-            <ImgWrap imageUrl={data.url} imageId={imageId} degree={Number(data?.degree ?? 0)} />
-            <StructuredDetail data={data} imageId={imageId} />
-          </div>
-        ) : <div className="h-500 w-full flex items-center justify-center"><Spin size="large" /></div>
+    const data = await api.image.fetchImageIndexes(params);
+    return data;
+  };
+  const fetchData = (id: string, toTime: number) => {
+    Promise.all([fetchImageIndexes(id), fetchImageJcds(id)]).then((res: any[]) => {
+      const [hData, jData] = res;
+      setHydData(hData.list.map(item => {
+        return ({ ...item, key: uuid() });
+      }));
+      // setJcdData(jData.list);
+      setJcdData(jData.list.map(item => {
+        return ({ ...item, key: uuid() });
+      }));
+      setImgData({ ...hData, imageId: id });
+      if (!isEmpty(jData.list)) {
+        fetchTemplate(jData.list[0].meta.createdTime, toTime);
+      } else {
+        setIsLoaded(true);
       }
-    </>
+    });
+  };
+
+  useEffect(() => {
+    const oTime = new Date().getTime();
+    setOpenTime(oTime);
+    fetchTemplate(0, oTime);
+    fetchData(imageId, oTime);
+    return () => {
+      setHydData([]);
+      setJcdData([]);
+      setTemplatePart({});
+      setIsLoaded(false);
+      setOpenTime(0);
+    };
+  }, []);
+  // console.log('templatePart223', templatePart);
+  // params.originIds = jcdData.map(item => item.meta.id);
+  return (
+    <div>
+      {
+        imgData ? (
+          <div className="flex justify-start items-start mt-10" style={{ minWidth: 1200 }}>
+            <ImgWrap
+              imageUrl={imgData.url}
+              handleClose={() => history.goBack()}
+              imageId={imgData.imageId}
+              degree={Number(imgData?.degree ?? 0)}
+            />
+            {
+              isLoaded && (
+                <StructuredDetail
+                  hydData={hydData}
+                  jcdData={jcdData}
+                  jcdOriginIds={jcdData.map(item => item.meta.id)}
+                  imageId={imgData?.imageId}
+                  handleRefresh={handleRefresh}
+                  handleClose={() => history.goBack()}
+                  templatePart={templatePart}
+                  tempAll={tempAll}
+                  openTime={openTime}
+                  // openTime={1632463461030}
+                />
+              )
+            }
+
+          </div>
+        ) : (
+          <div className="h-500 w-full flex items-center justify-center"><Spin size="large" /></div>
+        )
+      }
+      {
+        imgData && (
+          <div className="pl-18 flex">
+            <ExclamationCircleFilled style={{ color: '#FFCA4D', paddingTop: '4px' }} />
+            <div className="ml-3">
+              <p>1.如果图片内含多张单据，点击顶部选择图片类型</p>
+              <p>2.添加新的单据,每一张单据结构化后，一并保存</p>
+            </div>
+          </div>
+        )
+      }
+    </div>
   );
 };
-export default Structured;
+
+export default CheckImgStructured;
