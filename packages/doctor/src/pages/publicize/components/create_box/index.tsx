8856df689e09dca6e023e85b73de1fa0f6e72bdd
周素华 <zhoushzhou@gmail.com>
2021-12-03 14:02:29 +0800

    feat: 随访4.0.1crf文案调整

--- packages/doctor/src/pages/publicize/components/create_box/index.tsx
+++ packages/doctor/src/pages/publicize/components/create_box/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { useParams } from 'umi';
 import { PlusOutlined } from '@ant-design/icons';
 import styles from './index.scss';
+import { sfTypeUrl } from '../../utils';
 
 interface IProps {
   onClick: () => void;
 }
 function PatientEducation({ onClick }: IProps) {
   const { type } = useParams<{ type: string }>();
+
+
   return (
     <div className={styles.create_new} onClick={onClick}>
       <PlusOutlined />
-      <span>创建新{type === 'education' ? '宣教' : '随访'}</span>
+      <span>创建新{sfTypeUrl?.[type].text}</span>
     </div>
   );
 }
