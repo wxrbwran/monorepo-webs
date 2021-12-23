e7a772e87a08aef703514a553e5e4abde6583b45
wuxiaoran <wuxiaoran@xinzhili.cn>
2021-12-23 16:11:18 +0800

    fix(img): 统一单据前缀

--- packages/doctor/src/components/CheckImgStructured/compontents/IndexTable/components/RenderItem/index.tsx
+++ packages/doctor/src/components/CheckImgStructured/compontents/IndexTable/components/RenderItem/index.tsx
@@ -20,7 +20,7 @@ const RenderItem: FC<IProps> = (props) => {
   const sid = window.$storage.getItem('sid');
   useEffect(() => {
     // const
-    console.log('RenderItem', indexItem);
+    // console.log('RenderItem', indexItem);
     if (indexItem.referenceList) {
       setList(indexItem.referenceList);
     } else {
@@ -60,7 +60,7 @@ const RenderItem: FC<IProps> = (props) => {
             <Space align="baseline" style={{ display: 'flex' }}>
               <ProFormDependency name={[`${indexItem.formIndex}_${index}_reference`]}>
                 {(deps: any) => {
-                  console.log('deps', deps);
+                  // console.log('deps', deps);
                   const referenceId = deps[`${indexItem.formIndex}_${index}_reference`];
                   let disabled = true;
                   if (!indexItem?.references || indexItem?.references?.length === 0) {
