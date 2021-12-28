3843618db754a357f3e86076a17fc0f9bb4518b9
gx <gaoxue@xinzhili.cn>
2021-12-28 11:21:27 +0800

    fix: fix量表删除,增加ruleId参数

--- packages/cro/src/pages/end_event/detail/index.tsx
+++ packages/cro/src/pages/end_event/detail/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ScaleTableDetail from '@/components/ScaleTableDetail';
 
 function CrfDetail() {
-  return <ScaleTableDetail scaleType="CRF" />
+  return <ScaleTableDetail scaleType="CRF" />;
 }
 
 export default CrfDetail;
