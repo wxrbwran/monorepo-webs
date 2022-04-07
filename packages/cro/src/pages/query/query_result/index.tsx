b86a5d6672be066dcd76c020555f255e2bd13495
周素华 <zhoushzhou@gmail.com>
2022-04-07 18:25:41 +0800

    fix: 科研查询结果非首页也循环请求3次

--- packages/cro/src/pages/query/query_result/index.tsx
+++ packages/cro/src/pages/query/query_result/index.tsx
@@ -73,7 +73,7 @@ const QueryResult = forwardRef(({ param }: IProps, ref: Ref<any>) => {
 
     if ((results?.tableBody?.length ?? 0) < pageSize) {
 
-      if (page == 0 && retryTimes.current < 5) {
+      if (page == 0 && retryTimes.current < 5 || (page != 0 && retryTimes.current < 3)) {
         // 重试5次
         retryTimes.current += 1;
 
